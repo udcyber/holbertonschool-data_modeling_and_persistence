b3a7537019ff3dfe0c4a2d5e96068a015e73455a
udcyber <udewaleyne.pro@gmail.com>
2026-06-05 12:58:41 +0200

    corrected typos

--- sql_join/13-courses-above-average-assignments.sql
+++ sql_join/13-courses-above-average-assignments.sql
@@ -2,7 +2,7 @@ SELECT c.title AS course_title
 FROM courses c
 JOIN assignments a ON c.id = a.course_id
 GROUP BY c.title
-HAVING COUNT (a.id) > (
+HAVING COUNT(a.id) > (
     SELECT AVG(assign_count)
     FROM (
         SELECT COUT(*) AS assign_count
