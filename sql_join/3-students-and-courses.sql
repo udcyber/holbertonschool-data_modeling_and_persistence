68b6de6083f9f7a3096c2709ba6f82bb3c6836e8
udcyber <udewaleyne.pro@gmail.com>
2026-06-05 12:51:06 +0200

    changed JOIN to INNER JOIN

--- sql_join/3-students-and-courses.sql
+++ sql_join/3-students-and-courses.sql
@@ -1,5 +1,5 @@
 SELECT s.name AS student_name, c.title AS course_title
 FROM students s
-JOIN enrollments e ON s.id = e.student_id
-JOIN courses c ON e.course_id = c.id
+INNER JOIN enrollments e ON s.id = e.student_id
+INNER JOIN courses c ON e.course_id = c.id
 ORDER BY s.name, c.title;
