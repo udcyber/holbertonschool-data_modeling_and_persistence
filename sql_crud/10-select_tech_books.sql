b14e9c6d72a5466afdfbc3e6afa2ae92910f0bbb
udcyber <udewaleyne.pro@gmail.com>
2026-06-05 11:44:35 +0200

    changes inside IN

--- sql_crud/10-select_tech_books.sql
+++ sql_crud/10-select_tech_books.sql
@@ -1,4 +1,4 @@
 SELECT genre='Tech'
 FROM books
-WHERE published_year IN ('2000' OR published_year > '2000')
+WHERE published_year IN (published_year = '2000' OR published_year > '2000')
 GROUP BY title, price, stock;
