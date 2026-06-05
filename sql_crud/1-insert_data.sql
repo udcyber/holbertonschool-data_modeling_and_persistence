2e671d0e064d35fc7f243dbe8cdf446eae5b2b10
udcyber <udewaleyne.pro@gmail.com>
2026-06-05 10:54:20 +0200

    used one VALUES, put index in front, removed ' around numbers

--- sql_crud/1-insert_data.sql
+++ sql_crud/1-insert_data.sql
@@ -1,6 +1,7 @@
-INSERT INTO books
-VALUES ('The Hobbit', 'J.R.R. Tolkien', 'Fantasy', '10.99', '5', '1937'),
-VALUES ('1984', 'George Orwell', 'Dystopian', '8.99', '12', '1949'),
-VALUES ('Clean Code', 'Robert C. Martin', 'Tech', '32.50', '7', '2008'),
-VALUES ('The Pragmatic Programmer', 'Andrew Hunt', 'Tech', '28.75', '4', '1999')
-VALUES ('Dune', 'Frank Herbert', 'Sci-Fi', '9.50', '9', '1965');
+INSERT INTO books (id, title, author, genre, price, stock, published_year)
+VALUES
+(1, 'The Hobbit', 'J.R.R. Tolkien', 'Fantasy', 10.99, 5, 1937),
+(2, '1984', 'George Orwell', 'Dystopian', 8.99, 12, 1949),
+(3, 'Clean Code', 'Robert C. Martin', 'Tech', 32.50, 7, 2008),
+(4, 'The Pragmatic Programmer', 'Andrew Hunt', 'Tech', 28.75, 4, 1999),
+(5, 'Dune', 'Frank Herbert', 'Sci-Fi', 9.50, 9, 1965);
